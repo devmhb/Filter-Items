642103dddd59e0713c4505cf3621fb4955738118
Mehedi Bhuiyan <mehedibuiyan734@gmail.com>
2022-12-17 17:18:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Movie from "./Movie";
+import Filter from "./Filter";
 
 function App() {
+  const [popular, setPopular] = useState([]);
+  const [filterd, setFilterd] = useState([]);
+  const [genre, setGenre] = useState(0);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=b151e0bd713dec0cc2433669789a1150&language=en-US&page=1"
+    );
+    const moviesList = await data.json();
+    setPopular(moviesList.results);
+    setFilterd(moviesList.results);
+  };
+  console.log(popular);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Filter
+        popular={popular}
+        setFilterd={setFilterd}
+        genre={genre}
+        setGenre={setGenre}
+      />
+      <div className="App">
+        {filterd.map((movie) => {
+          return <Movie key={movie.id} movie={movie} />;
+        })}
+      </div>
+    </>
   );
 }
 
---
+++ src/Filter.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+
+const Filter = ({ popular, setFilterd, genre, setGenre }) => {
+  useEffect(() => {
+    if (genre === 0) {
+      setFilterd(popular);
+      return;
+    }
+
+    const filteredItem = popular.filter((movie) =>
+      movie.genre_id.includes(genre)
+    );
+    setFilterd(filteredItem);
+  }, [genre]);
+
+  return (
+    <div className="btnCon">
+      <button onClick={() => setGenre(0)}>All</button>
+      <button onClick={() => setGenre(35)}>Comedy</button>
+      <button onClick={() => setGenre(28)}>Action</button>
+    </div>
+  );
+};
+
+export default Filter;
